9d2d48ffcf6a0e3ebd71f88ca70c98f34c53a0dc
Mihaela Angelova <mihaela.yankova7@gmail.com>
2024-01-06 16:12:24 +0200

    Get request for /home

--- ecommerce-backend/insertProducts.sql
+++ ecommerce-backend/insertProducts.sql
@@ -3,6 +3,6 @@ DELETE FROM web_order;
 DELETE FROM product;
 DELETE FROM address;
 
-INSERT INTO product ([name], short_description, long_description, price, product_type, quantity) VALUES ('Opeth T-shirt Heritage', 'T-shirt', 'Opeth T-shirt of the album Heritage.', 30, 0, 5);
-INSERT INTO product ([name], short_description, long_description, price, product_type, quantity) VALUES ('Opeth mug Deliverance', 'Mug', 'Opeth mug of the album Deliverance.', 15, 1, 12);
-INSERT INTO product ([name], short_description, long_description, price, product_type, quantity) VALUES ('Opeth CD Damnation', 'CD', 'Opeth CD of the album Damnation.', 20, 2, 3);
+INSERT INTO product ([name], description, price, product_type, quantity) VALUES ('Opeth T-shirt Heritage', 'Opeth T-shirt of the album Heritage.', 30, 0, 5);
+INSERT INTO product ([name], description, price, product_type, quantity) VALUES ('Opeth mug Deliverance', 'Opeth mug of the album Deliverance.', 15, 1, 12);
+INSERT INTO product ([name], description, price, product_type, quantity) VALUES ('Opeth CD Damnation', 'Opeth CD of the album Damnation.', 20, 2, 3);
---
+++ ecommerce-backend/src/main/java/com/project/ecommercebackend/api/controller/home/HomeController.java
@@ -0,0 +1,26 @@
+package com.project.ecommercebackend.api.controller.home;
+
+import com.project.ecommercebackend.model.Product;
+import com.project.ecommercebackend.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    private ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> homepage() {
+        return productService.getProducts();
+    }
+
+}
