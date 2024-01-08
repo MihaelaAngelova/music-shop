c0a0ea7cbf45c3371282eeeef53cece7e6d49705
Mihaela Angelova <mihaela.yankova7@gmail.com>
2024-01-09 00:50:24 +0200

    Get request for /cart

--- ecommerce-backend/src/main/java/com/project/ecommercebackend/api/controller/cart/CartController.java
+++ ecommerce-backend/src/main/java/com/project/ecommercebackend/api/controller/cart/CartController.java
@@ -5,10 +5,7 @@ import com.project.ecommercebackend.service.ProductService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -28,12 +25,21 @@ public class CartController {
         if(!productService.decreaseQuantity(productQuantityPair.getProductId(), productQuantityPair.getQuantity())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-        ArrayList<ProductQuantityPair> cart = (ArrayList<ProductQuantityPair>) session.getAttribute("Cart");
+        List<ProductQuantityPair> cart = (List<ProductQuantityPair>) session.getAttribute("Cart");
         if (cart == null) {
             cart = new ArrayList<>();
         }
         cart.add(productQuantityPair);
         session.setAttribute("Cart", cart);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public List<ProductQuantityPair> getCart(HttpSession session) {
+        List<ProductQuantityPair> cart = (List<ProductQuantityPair>) session.getAttribute("Cart");
+        if(cart == null) {
+            return new ArrayList<>();
+        }
+        return cart;
     }
 }
