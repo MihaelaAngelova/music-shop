db714419d4c9fbcbeff002d0e322d9564764f724
Mihaela Angelova <mihaela.yankova7@gmail.com>
2024-01-09 01:08:03 +0200

    Delete request for /cart/{id}

--- ecommerce-backend/src/main/java/com/project/ecommercebackend/api/controller/cart/CartController.java
+++ ecommerce-backend/src/main/java/com/project/ecommercebackend/api/controller/cart/CartController.java
@@ -7,13 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/cart")
 public class CartController {
+    private final static String CART = "Cart";
     private ProductService productService;
 
     public CartController(ProductService productService) {
@@ -25,21 +25,36 @@ public class CartController {
         if(!productService.decreaseQuantity(productQuantityPair.getProductId(), productQuantityPair.getQuantity())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-        List<ProductQuantityPair> cart = (List<ProductQuantityPair>) session.getAttribute("Cart");
+        List<ProductQuantityPair> cart = (List<ProductQuantityPair>) session.getAttribute(CART);
         if (cart == null) {
             cart = new ArrayList<>();
         }
         cart.add(productQuantityPair);
-        session.setAttribute("Cart", cart);
+        session.setAttribute(CART, cart);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping
     public List<ProductQuantityPair> getCart(HttpSession session) {
-        List<ProductQuantityPair> cart = (List<ProductQuantityPair>) session.getAttribute("Cart");
+        List<ProductQuantityPair> cart = (List<ProductQuantityPair>) session.getAttribute(CART);
         if(cart == null) {
             return new ArrayList<>();
         }
         return cart;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProductFromCart(@PathVariable int id, HttpSession session) {
+        List<ProductQuantityPair> cart = (List<ProductQuantityPair>) session.getAttribute(CART);
+        if(cart == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        boolean productExists = cart.stream()
+                .anyMatch(pair -> pair.getProductId() == id);
+        if(productExists) {
+            cart = cart.stream().filter(pair -> pair.getProductId() != id).toList();
+            session.setAttribute(CART, cart);
+            return ResponseEntity.ok().build();
+        } else return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
